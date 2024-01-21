83c727ffc431dd39aa70e287a5ba0f3dff199dec
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-01-20 21:27:23 -0500

    Fix bug, removw counter

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 def count_intersections(chords):
-    lines = dict()
+    lines = []
     result = 0
     length = int(len(chords[0]) / 2)
-    counter = 1
     
     for i in range(length):
-        lines[i] = [chords[0][i], chords[0][length + i]]
+        lines.append([chords[0][i], chords[0][length + i]])
 
     print(lines)
         
@@ -16,16 +15,17 @@ def count_intersections(chords):
         e1 = lines[p1][1]
         s2 = lines[p2][0]
         e2 = lines[p2][1]
-        # print()
-        if s2 > s1 and s2 < e1 or e2 > s1 and e2 < e1:
-            result += counter
-            counter += 1
         
-        if p2 < len(lines):
+        print("p1:", p1, "p2:", p2)
+        
+        if s2 > s1 and s2 < e1 or e2 > s1 and e2 < e1:
+            result += 1
+        
+        if p2 < len(lines) - 1:
+            p2 += 1
+        else:
             p1 += 1
             p2 = p1+1
-        else:
-            p2 += 1
        
         
     return result
@@ -33,6 +33,7 @@ def count_intersections(chords):
     
 # chords = [(0.78, 1.47, 1.77, 3.92, 4, 4.1, 7.2, 8, 9, 10), ('s1', 's2', 's3', 's4', 'e1', 'e2', 'e3', 'e4')]
 # chords = [(1, 3.2, 6, 3, 5, 9), ('s1', 's2', 's3', 's4', 'e1', 'e2', 'e3', 'e4')]
-chords = [(1, 4, 5, 3, 6, 7), ('s1', 's2', 's3', 's4', 'e1', 'e2', 'e3', 'e4')]
+# chords = [(1, 4, 5, 3, 6, 7), ('s1', 's2', 's3', 's4', 'e1', 'e2', 'e3', 'e4')]
+# chords = [(0.78, 1.47, 1.77, 3.92), ('s1', 's2', 'e1', 'e2')]
 result = count_intersections(chords)
 print("Number of intersections:", result)
