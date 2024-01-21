f9c7bff096d1129386147760b3db8d40a2ce280b
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-01-21 11:23:02 -0500

    Add simple frontend

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-# app.py
 from flask import Flask, render_template
+import subprocess
+import sys
 
 app = Flask(__name__)
 
@@ -7,5 +8,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/process', methods=['POST'])
+def process():
+    try:
+        # Run main.py using subprocess
+        result_message = subprocess.check_output([sys.executable, 'main.py'], universal_newlines=True)
+    except subprocess.CalledProcessError as e:
+        result_message = f"Error executing Python code: {e}"
+
+    return render_template('index.html', result=result_message)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
--- main.py
+++ main.py
@@ -27,9 +27,15 @@ def count_intersections(chords):
     return intersections
     
     
-chords = [(0.78, 1.47, 1.77, 3.92, 4, 4.1, 7.2, 8, 9, 10), ('s1', 's2', 's3', 's4', 'e1', 'e2', 'e3', 'e4')]
+# chords = [(0.78, 1.47, 1.77, 3.92, 4, 4.1, 7.2, 8, 9, 10), ('s1', 's2', 's3', 's4', 'e1', 'e2', 'e3', 'e4')]
 # chords = [(1, 3.2, 6, 3, 5, 9), ('s1', 's2', 's3', 'e1', 'e2', 'e3')]
 # chords = [(1, 4, 5, 3, 6, 7), ('s1', 's2', 's3', 'e1', 'e2', 'e3')]
-# chords = [(0.78, 1.47, 1.77, 3.92), ('s1', 's2', 'e1', 'e2')]
-result = count_intersections(chords)
-print("Number of intersections:", result)
+chords = [(0.78, 1.47, 1.77, 3.92), ('s1', 's2', 'e1', 'e2')]
+try:
+    # Calculate the result
+    result = count_intersections(chords)
+    print(f"Number of intersections: {result}")
+
+except Exception as e:
+    print(f"Error: {e}")
+    result = None
